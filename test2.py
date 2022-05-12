3ba925c47150a6e94e9cabf0c2dc35232de65515
o120d6@gmail.com <o120d6@gmail.com>
2022-05-12 02:51:34 +0200

    end of the day, made algorithm for paths, but it's not that good no curves it would be better probably to convert path from png

--- test2.py
+++ test2.py
@@ -1,8 +1,54 @@
-import random, sys, copy, os, pygame
+import random, sys, copy, os, pygame, csv
 import pygame
 
 
 # def create_path():
+def save_path(path):
+    with open('arena1_path.csv', 'w', newline='', encoding='UTF8') as f:
+        # create the csv writer
+        writer = csv.writer(f, delimiter=';')
+
+        # write a row to the csv file
+        writer.writerows(path)
+
+
+def read_path():
+    path = []
+    with open('arena1_path.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        for row in csv_reader:
+            path.append((int(row[0]), int(row[1])))
+    return path
+
+
+def point_to_point_path_random_algorythm_by_myself(point1: [int, int], point2: [int, int]) -> [(int, int)]:
+    # I dont care if it is not optimal, Im just gonna generate few maps nobody cares, and internet tell no answer to me
+    # This algorithm calculate path points in straight line from point to point without first starting point
+    path = []
+    # path.append(point1[0],point1[1])
+    difference_x = max(point1[0], point2[0]) - min(point1[0], point2[0])
+    difference_y = max(point1[1], point2[1]) - min(point1[1], point2[1])
+    points_count = max(difference_x, difference_y)
+    direction = [-1, 0] if point1[0] - point2[0] > 0 else [1, 0]
+    direction[1] = -1 if point1[1] - point2[1] > 0 else 1
+    while not (round(point1[0]) == point2[0] and round(point1[1]) == point2[1]):
+        point1[0] += direction[0] * difference_x / points_count
+        point1[1] += direction[1] * difference_y / points_count
+
+        path.append((round(point1[0]), round(point1[1])))
+    return path
+
+
+def process_path(path):
+    complex_path = []
+    old_point = []
+    for point in path:
+        if len(old_point) != 0:
+            complex_path += point_to_point_path_random_algorythm_by_myself(list(old_point), list(point))
+        else:
+            complex_path.append(point)
+        old_point = point
+    return complex_path
 
 
 def start():
@@ -35,10 +81,12 @@ def start():
                 clicked_sprites = [s for s in sprites if s.rect.collidepoint(pos)]
                 # do something with the clicked sprites...
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    print("You have pressed right arrow key")
-                elif event.key == pygame.K_LEFT:
-                    print("You have pressed left arrow key")
+                if event.key == pygame.K_i:
+                    save_path(path)
+                elif event.key == pygame.K_o:
+                    path = read_path()
+                elif event.key == pygame.K_p:
+                    path = process_path(path)
 
         # Fill the background with white
         screen.fill((255, 3, 255))
@@ -48,10 +96,10 @@ def start():
         # Draw a green line
         pygame.draw.line(screen, (0, 0, 255), (0, 0), (900, 500), 5)
         screen.blit(arena1, (0, 0))
-        #draw path
+        # draw path
         last_point = []
         for point in path:
-            pygame.draw.circle(screen, (100,100,100),(point[0],point[1]),10)
+            pygame.draw.circle(screen, (100, 100, 100), (point[0], point[1]), 10)
             if len(last_point) != 0:
                 pygame.draw.line(screen, (0, 0, 255), point, last_point, 5)
             last_point = point;
